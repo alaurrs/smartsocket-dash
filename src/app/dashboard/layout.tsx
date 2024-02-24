404e073b2e3e9165ae2bd55dd86560005a8ade0f
alaurrs <sallyvann.ange@edu.esiee.fr>
2024-02-24 19:29:55 +0100

    first commit

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import SideNav from "@/app/ui/sidenav";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,8 +16,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <html lang="en">
-        <body className={`${inter.className} antialiased bg-gray-100`}>{children}</body>
-      </html>
+    <html lang="en">
+
+    <body className={inter.className}>
+
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-64">
+          <SideNav/>
+        </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      </div>
+
+    </body>
+    </html>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import Login from "@/app/login/login";
 
 export default function Home() {
   return (
       <h1 className={"text-3xl"}>
         Smartsocket Dash
-          <Login/>
       </h1>
   );
 }
